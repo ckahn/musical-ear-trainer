e6cde4500bfe47dee950a0466dae85a94dfde899
Chris Kahn <ckahn@users.noreply.github.com>
2014-07-13 18:08:27 -0700

    Moved class into separate file.

---
+++ MusicalEarTrainer.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/** 
+ * An interactive keyboard that you can play with a mouse.
+ *
+ */
+public class MusicalEarTrainer extends JFrame implements ActionListener {
+	
+	Keyboard keyboard = new Keyboard();
+	JButton playButton = new JButton("Play");
+	
+	// Initialize window and add keyboard
+	private MusicalEarTrainer() {
+		setTitle("Interactive Keyboard");
+		setSize(45*15+1, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JTextArea area = keyboard.area;
+				
+		getContentPane().add(playButton, BorderLayout.PAGE_START);
+		getContentPane().add(keyboard, BorderLayout.CENTER);
+		keyboard.area.setLineWrap(true);
+		getContentPane().add(area, BorderLayout.PAGE_END);
+				
+		playButton.addActionListener(this);
+	}
+	
+	public void actionPerformed (ActionEvent ev) {
+		if (ev.getSource() == playButton) {
+			keyboard.playMelody();
+		}
+	}
+
+	// Create and show application window
+	public static void main(String[] args) {
+
+		// Code is run on an event-dispatching thread
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				MusicalEarTrainer window = new MusicalEarTrainer();
+				window.setVisible(true);
+			}
+		});   
+	}
+}
